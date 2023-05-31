af7eb31538104ea299074656d90d41e8b82d55b6
Jacky <mygtwrx@gmail.com>
2023-06-01 01:00:12 +0800

    feat: SQL unit

--- sqlconn.py
+++ sqlconn.py
@@ -2,106 +2,89 @@
 # 创建表
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-print ("数据库打开成功")
-c = conn.cursor()
-c.execute('''CREATE TABLE COMPANY
-       (ID INT PRIMARY KEY     NOT NULL,
-       NAME           TEXT    NOT NULL,
-       AGE            INT     NOT NULL,
-       ADDRESS        CHAR(50),
-       SALARY         REAL);''')
-print ("数据表创建成功")
-conn.commit()
-conn.close()
+def createSQL() -> None:
+    conn = sqlite3.connect('test.db') # Create if it doesn't exist.
+    c = conn.cursor() # ctrl
+    print ("数据库打开成功")
+    c.execute('''CREATE TABLE COMPANY
+        (ID INT PRIMARY KEY     NOT NULL,
+        NAME           TEXT    NOT NULL,
+        AGE            INT     NOT NULL,
+        ADDRESS        CHAR(50),
+        SALARY         REAL);''')
+    print ("数据表创建成功")
+    conn.commit()
+    conn.close()
 
 
-#!/usr/bin/python
-# INSERT 操作
-import sqlite3
+def addSQL() -> None:
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    print ("数据库打开成功")
+    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (1, 'Paul', 32, 'California', 20000.00 )")
 
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-print ("数据库打开成功")
+    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
 
-c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (1, 'Paul', 32, 'California', 20000.00 )")
+    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
 
-c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
+    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+        VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+    conn.commit()
+    print ("数据插入成功")
+    conn.close()
 
-c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
+def selectSQL() -> None:
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    print ("数据库打开成功")
 
-c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+    cursor = c.execute("SELECT id, name, address, salary  from COMPANY")
+    for row in cursor:
+        print( "ID = ", row[0])
+        print( "NAME = ", row[1])
+        print( "ADDRESS = ", row[2])
+        print( "SALARY = ", row[3], "\n")
 
-conn.commit()
-print ("数据插入成功")
-conn.close()
+    print ("数据操作成功")
+    conn.close()
 
+def updateSQL() -> None:
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    print ("数据库打开成功")
 
+    c.execute("UPDATE COMPANY set SALARY = 25000.00 where ID=1")
+    conn.commit()
+    print ("Total number of rows updated :", conn.total_changes)
 
-# SELECT 操作
-import sqlite3
+    cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
+    for row in cursor:
+        print( "ID = ", row[0])
+        print( "NAME = ", row[1])
+        print( "ADDRESS = ", row[2])
+        print( "SALARY = ", row[3], "\n")
 
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-print ("数据库打开成功")
+    print ("数据操作成功")
+    conn.close()
 
-cursor = c.execute("SELECT id, name, address, salary  from COMPANY")
-for row in cursor:
-   print( "ID = ", row[0])
-   print( "NAME = ", row[1])
-   print( "ADDRESS = ", row[2])
-   print( "SALARY = ", row[3], "\n")
+def deleteSQL() -> None:
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    print ("数据库打开成功")
 
-print ("数据操作成功")
-conn.close()
+    c.execute("DELETE from COMPANY where ID=2;")
+    conn.commit()
+    print ("Total number of rows deleted :", conn.total_changes)
 
+    cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
+    for row in cursor:
+        print ("ID = ", row[0])
+        print ("NAME = ", row[1])
+        print ("ADDRESS = ", row[2])
+        print ("SALARY = ", row[3], "\n")
 
-
-# UPDATE 操作
-#!/usr/bin/python
-import sqlite3
-
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-print ("数据库打开成功")
-
-c.execute("UPDATE COMPANY set SALARY = 25000.00 where ID=1")
-conn.commit()
-print ("Total number of rows updated :", conn.total_changes)
-
-cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
-for row in cursor:
-   print( "ID = ", row[0])
-   print( "NAME = ", row[1])
-   print( "ADDRESS = ", row[2])
-   print( "SALARY = ", row[3], "\n")
-
-print ("数据操作成功")
-conn.close()
-
-
-# DELETE 操作
-#!/usr/bin/python
-import sqlite3
-
-conn = sqlite3.connect('test.db')
-c = conn.cursor()
-print ("数据库打开成功")
-
-c.execute("DELETE from COMPANY where ID=2;")
-conn.commit()
-print ("Total number of rows deleted :", conn.total_changes)
-
-cursor = conn.execute("SELECT id, name, address, salary  from COMPANY")
-for row in cursor:
-   print ("ID = ", row[0])
-   print ("NAME = ", row[1])
-   print ("ADDRESS = ", row[2])
-   print ("SALARY = ", row[3], "\n")
-
-print ("数据操作成功")
-conn.close()
+    print ("数据操作成功")
+    conn.close()
